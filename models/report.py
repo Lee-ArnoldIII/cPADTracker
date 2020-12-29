a4a7bf9b26dfac5b2acf49ac7a0aa29880f13588
Lee Arnold <lee.arnoldiii@gmail.com>
2020-12-29 15:19:51 -0600

    updated several files for the CRUD functions to work for reports. Still working on get for single report. Going to push up what is here and then do a nother commit later today.

--- models/report.py
+++ models/report.py
@@ -1,33 +1,35 @@
 from db import db
 #TODO: Add following:
 # 1) remaining parts of model
-# 2) delete from db function
+
 # 3) update json function
 
 class ReportModel(db.Model):
     __tablename__ = 'reports'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
     benchmark = db.Column(db.String(80))
     content = db.Column(db.Text)
     status = db.Column(db.String(80))
     
         
-    def __init__(self, benchmark, content, status):
+    def __init__(self, name, benchmark, content, status):
+        self.name = name
         self.benchmark = benchmark
         self.content = content
         self.status = status
         
      
     def json(self):
-        return {'benchmark': self.benchmark,'content': self.content, 
+        return {'name': self.name, 'benchmark': self.benchmark,'content': self.content, 
                 'status': self.status}
         # may need to do a list comprehension for this function to show all reports for a selected user
 
    
     @classmethod
-    def find_by_id(cls, _id):
-        return cls.query.filter_by(id=_id).all()
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).all()
 
     def save_to_db(self):
         db.session.add(self)
--- resources/report.py
+++ resources/report.py
@@ -1,80 +1,51 @@
-from flask_restful import Resource, reqparse
 from flask_jwt import JWT, jwt_required
+from flask_restful import Resource, reqparse
 from models.report import ReportModel
-
-
-#TODO: Add following:
-# 1) parser arguments from model
-# 2) all CRUD methods
-# 3) test to make sure this doesn't jack up JWT
 
 class Report(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('benchmark', 
             type=str,
-            required=True,
-            help="This field cannot be blank!"
+            help="This cannot be blank!"
     )
     parser.add_argument('content', 
             type=str,
-            required=True,
-            help="This field cannot be blank!"
+            help="This cannot be blank!"
     )
     parser.add_argument('status', 
             type=str,
-            required=True,
-            help="This field cannot be blank!"
+            help="This cannot be blank!"
     )
 
     @jwt_required()
-    def get(self, id):
-        report = ReportModel.find_by_id(id)
+    def get(self, name):
+        report = ReportModel.find_by_name(name)
         if report:
             return report.json()
-        return {'message': 'Report not found!'}, 404
+        return {'message': "Report not found!"}, 404
 
-    def post(self, id):
-        if ReportModel.find_by_id(id):
-            return {'message': "That report already exists!"}, 400
+    def post(self, name):
+        if ReportModel.find_by_name(name):
+            return {'message': "A report with that id already exists!"}, 400
         
         data = Report.parser.parse_args()
 
-        report = ReportModel(id, **data)
+        report = ReportModel(name, **data)
 
         try:
             report.save_to_db()
-        except:
-            return {'message': "An error occured creating the task."}, 500
+        except: 
+            return {'message': "An error occured creating the report!"}, 500
 
-        return report.json()
+        return {'message': "Report created successfully."}, 201
 
-    # def delete(self, name):
-    #     task = TaskModel.find_by_name(name)
-    #     if task is None:
-    #         return {'message': 'Task not found!'}, 404
-    #     else:
-    #         task.delete_from_db()
-    #         return {'message': 'Task deleted.'}
+    def put(self, name):
+        pass
 
-    # def put(self, name):
-    #     data = Task.parser.parse_args()
-
-    #     task = TaskModel.find_by_name(name)
-
-    #     if task is None:
-    #         task = TaskModel(name, **data)
-    #     else: 
-    #         task.name = name
-    #         task.description = data['description']
-        
-    #     try:
-    #         task.save_to_db()
-    #     except:
-    #         return {'message': "An error occured."}, 500
-        
-    #     return task.json()
+    def delete(self, name):
+        pass
 
 
 class ReportList(Resource):
     def get(self):
-        return {'report': [report.json() for report in ReportModel.query.all()]}
+        return {'reports': [report.json() for report in ReportModel.query.all()]}
